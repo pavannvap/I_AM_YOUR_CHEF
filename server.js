7e9f441ae6e8c591a311b6f79ba083a7bb8d797e
pavan nimmaraju <pavan@pavans-MacBook-Pro.local>
2026-04-15 16:54:59 -0400

    updated server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
 
 app.get('/', (req, res) => {
-  res.send('API Running...');
+  res.send('API updated and integrated CI/CD');
 });
 
 const PORT = process.env.PORT || 3000;
